3fa034db6372249fc80ac9a356af84e71f53639a
Jerry Reptak <jreptak@gmail.com>
2013-05-02 22:29:27 -0400

    Cleaner js

--- public/js/register.js
+++ public/js/register.js
@@ -1,24 +1,30 @@
-displayAffiliation();
-document.getElementById('affiliationDropdown').onchange = displayAffiliation;
+$(function(){
+  var affiliation = $('#affiliation');
+  var dropdown = $('#affiliationDropdown');
 
-function displayAffiliation() {
-    var dropdown = document.getElementById('affiliationDropdown');
-    if(dropdown.value == 'Student') {
-        displayForStudent();
-    } else {
-        displayForFaculty();
-    }
-}
-function displayForStudent(){
-    $('#affiliation').empty();
-    $('#affiliation').append($('#student-display').html());
-    $('#major-add-row').click(function(){
-        $('#major-div').append($('#major-div-sub-row').html());
-    });
-    $('#major-div').on('click','.major-sub-row', function(){
-        $(this).parent().parent().remove();
-    });
-}
-function displayForFaculty(){
-    $('#affiliation').empty();
-} 
+  function displayForStudent(){
+      affiliation.empty();
+      affiliation.append($('#student-display').html());
+      $('#major-add-row').click(function(){
+          $('#major-div').append($('#major-div-sub-row').html());
+      });
+      $('#major-div').on('click','.major-sub-row', function(){
+          $(this).parent().parent().remove();
+      });
+  }
+
+  function displayForFaculty(){
+      affiliation.empty();
+  } 
+
+  function displayAffiliation() {
+      if(dropdown.val() === 'student') {
+          displayForStudent();
+      } else {
+          displayForFaculty();
+      }
+  }
+
+  displayAffiliation();
+  dropdown.on('change', displayAffiliation);
+});
